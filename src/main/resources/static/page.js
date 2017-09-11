d34781205ad92122b165112f1c30845503b10535
Roman Galanti <roman.galanti@libertymutual.com>
2017-09-11 10:54:09 -0700

    Added recipe output and submit forms.

--- src/main/resources/static/page.js
+++ src/main/resources/static/page.js
@@ -1,5 +1,32 @@
 const baseurl = 'http://localhost:8080/recipes';
 
+$('#create-recipe-form').on('submit', function (e) {
+	e.preventDefault();
+	
+	let payload = {
+		title: $('#title').val(),
+		description: $('#description').val(),
+		numberOfMinutes: $('#numberOfMinutes').val(),
+		url: $('#url').val()
+	};
+	
+	let ajaxOptions = {
+		type: 'POST',
+		data: JSON.stringify(payload),
+		contentType: 'application/json'
+	};
+	
+	$.ajax(this.action, ajaxOptions)
+		.done(function (recipe) {
+			$('<li></li>')
+			.html('<a href="#" data-recipe-id="' + recipe.id + '">' + recipe.title + '</a>')
+			.appendTo($('#recipe-list'));
+		})
+		.fail(function (error) {
+			console.log(error);
+		});
+});
+
 $(document).on('click', 'a[data-recipe-id]', function (e) {
 	e.preventDefault();
 	const recipeId = $(this).data('recipeId');
@@ -11,6 +38,7 @@ $(document).on('click', 'a[data-recipe-id]', function (e) {
 					<h1>Title: ${data.title}</h1>
 					<h2>Description: ${data.description}</h2>
 					<div>Number of Minutes: ${data.numberOfMinutes}</div>
+					<div>Picture Url: ${data.url}</div>
 			`);
 	}) 
 });
